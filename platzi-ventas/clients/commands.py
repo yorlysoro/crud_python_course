4c48fb722a84b220d5a0ac8ed0839b031f7e7a63
yorlysoro <yorlysoro@gmail.com>
2022-08-11 18:35:45 -0400

    fix some errors add tabulate in print

--- platzi-ventas/clients/commands.py
+++ platzi-ventas/clients/commands.py
@@ -1,4 +1,5 @@
 import click
+from tabulate import tabulate
 
 from clients.services import ClientService
 from clients.models import Client
@@ -41,16 +42,19 @@ def list(ctx):
     """List all clients"""
     client_service = ClientService(ctx.obj['clients_table'])
     clients = client_service.list_clients()
-    click.echo('ID  |   Name    |   Company     |   Email   |   Position')
-    click.echo('-' * 100)
+    headers = [field.capitalize() for field in Client.schema()]
+    table = []
+    # click.echo('ID  |   Name    |   Company     |   Email   |   Position')
+    # click.echo('-' * 100)
     for client in clients:
-        click.echo('{uid}  |   {name}  |   {company}   |   {email}    |    {position} '.format(
-            uid=client['uid'],
-            name=client['name'],
-            company=client['company'],
-            email=client['email'],
-            position=client['position']
-        ))
+        table.append([
+            client['uid'],
+            client['name'],
+            client['company'],
+            client['email'],
+            client['position']
+        ])
+    click.echo(tabulate(table, headers))
 
 
 @clients.command()
